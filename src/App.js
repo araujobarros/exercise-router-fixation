232cee67ac886792602150417672b8e17da0d4c6
Edson Araujo <araujo.barros@matematica.ufrj.br>
2021-01-21 18:18:43 -0300

    Fixaçao- 1 ao 6

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
+import { BrowserRouter, Link, Route } from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import Users from './Users';
 
 class App extends Component {
   render() {
     return (
-      <Home />
+      <BrowserRouter>
+        <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/users' component={Users} />
+      </BrowserRouter>
     );
   }
 }
--- src/Users.js
+++ src/Users.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Users = () => (
   <div>
     <h2> Users </h2>
     <p> My awesome Users component </p>
+    <Link to='/' >Home</Link> <br />
   </div>
+  
 );
 
 export default Users;
